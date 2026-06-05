cf3c1077a6ef9d503777b65f6d0c75010778e478
Deepraj Kashyap <deeprajkashyap21@gmail.com>
2026-06-05 15:47:58 +0530

    fix register schema

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,335 +1,254 @@
-import {useState} from "react"
+import { useState } from "react"
 import API from "../api/axios"
-import {useNavigate, Link} from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 
 
-function Register(){
+function Register() {
+
+    const navigate = useNavigate()
 
 
-const navigate = useNavigate()
+    const [form, setForm] = useState({
 
-
-const [form,setForm] = useState({
-
-    name:"",
-    email:"",
-    password:"",
-    education:"",
-    designation:"",
-    interests:""
-
-})
-
-
-
-
-
-
-const handleChange = (e)=>{
-
-
-    setForm({
-
-        ...form,
-
-        [e.target.name]:e.target.value
+        name: "",
+        email: "",
+        password: "",
+        education: "",
+        designation: "",
+        interests: ""
 
     })
 
 
-}
+    const handleChange = (e) => {
+
+        setForm({
+
+            ...form,
+            [e.target.name]: e.target.value
+
+        })
+
+    }
 
 
 
 
+    const register = async () => {
+
+        try {
+
+            const userData = {
+
+                name: form.name,
+                email: form.email,
+                password: form.password,
+                education: form.education,
+                designation: form.designation,
+
+                interests: form.interests
+                    .split(",")
+                    .map(item => item.trim())
+
+            }
 
 
+            console.log(userData)
 
 
+            await API.post(
+                "/auth/register",
+                userData
+            )
 
-const register = async()=>{
+
+            navigate("/login")
 
 
-    await API.post(
+        } catch (error) {
 
-        "/auth/register",
+            console.log(error.response?.data)
 
-        {   name: form.name,
-            email: form.email,
-            password: form.password,
-            education: form.education,
-            designation: form.designation,
-            interests: form.interests.split(",")
+            alert(
+                error.response?.data?.detail ||
+                "Registration failed"
+            )
+
         }
+
+    }
+
+
+
+
+
+
+
+    return (
+
+        <div
+            className="
+            min-h-[85vh]
+            flex
+            items-center
+            justify-center
+            pt-16
+            pb-10
+            "
+        >
+
+
+            <div
+                className="
+                w-105
+                bg-slate-800
+                border
+                border-slate-700
+                rounded-2xl
+                p-10
+                shadow-[0_0_40px_rgba(37,99,235,0.15)]
+                "
+            >
+
+
+
+                <h1
+                    className="
+                    text-4xl
+                    font-light
+                    tracking-wide
+                    mb-3
+                    "
+                >
+
+                    Join NEXUS
+
+                </h1>
+
+
+
+
+
+                <input
+                    name="name"
+                    placeholder="Name"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+                <input
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+                <input
+                    name="education"
+                    placeholder="Education"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+                <input
+                    name="designation"
+                    placeholder="Designation"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+                <input
+                    name="interests"
+                    placeholder="Interests (VLSI, processors, CPU)"
+                    onChange={handleChange}
+                    className="input-style"
+                />
+
+
+
+
+                <button
+
+                    onClick={register}
+
+                    className="
+                    w-full
+                    bg-blue-600
+                    py-3
+                    rounded-xl
+                    font-medium
+                    hover:bg-blue-700
+                    transition
+                    mt-3
+                    "
+
+                >
+
+                    Create Account
+
+                </button>
+
+
+
+
+
+
+                <p
+                    className="
+                    text-center
+                    text-gray-400
+                    font-light
+                    mt-6
+                    "
+                >
+
+                    Already a user?{" "}
+
+
+                    <Link
+                        to="/login"
+
+                        className="
+                        text-blue-400
+                        hover:text-blue-500
+                        transition
+                        "
+                    >
+
+                        Login
+
+                    </Link>
+
+
+                </p>
+
+
+            </div>
+
+
+        </div>
+
     )
 
-
-    navigate("/login")
-
-
 }
-
-
-
-
-
-
-return (
-
-<div
-className="
-min-h-[85vh]
-flex
-items-center
-justify-center
-pt-16
-pb-10
-"
->
-
-
-
-<div
-className="
-w-105
-bg-slate-800
-border
-border-slate-700
-rounded-2xl
-p-10
-shadow-[0_0_40px_rgba(37,99,235,0.15)]
-"
->
-
-
-
-
-
-<h1
-className="
-text-4xl
-font-light
-tracking-wide
-mb-3
-"
->
-
-Join NEXUS
-
-</h1>
-
-
-
-
-
-<p
-className="
-text-gray-400
-font-light
-mb-8
-"
->
-
-
-
-</p>
-
-
-
-
-
-
-
-
-<input
-
-name="name"
-
-placeholder="Name"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-<input
-
-name="email"
-
-placeholder="Email"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-<input
-
-type="password"
-
-name="password"
-
-placeholder="Password"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-
-<input
-
-name="education"
-
-placeholder="Education"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-
-<input
-
-name="designation"
-
-placeholder="Designation"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-
-
-<input
-
-name="interests"
-
-placeholder="Interests (VLSI, processors, CPU)"
-
-onChange={handleChange}
-
-className="input-style"
-
-/>
-
-
-
-
-
-
-
-
-
-<button
-
-onClick={register}
-
-className="
-w-full
-bg-blue-600
-py-3
-rounded-xl
-font-medium
-hover:bg-blue-700
-transition
-mt-3
-"
-
->
-
-Create Account
-
-</button>
-
-
-
-
-
-
-
-
-
-<p
-className="
-text-center
-text-gray-400
-font-light
-mt-6
-"
->
-
-
-Already a user?{" "}
-
-
-<Link
-
-to="/login"
-
-className="
-text-blue-400
-hover:text-blue-500
-transition
-"
-
->
-
-Login
-
-</Link>
-
-
-
-</p>
-
-
-
-
-
-</div>
-
-
-
-</div>
-
-
-)
-
-
-}
-
 
 
 export default Register
