224b19a22bddfd737f03dbc65a6c9c7b105fd001
Deepraj Kashyap <deeprajkashyap21@gmail.com>
2026-06-05 14:18:29 +0530

    fix cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,7 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi import FastAPI
-
 from app.routes import auth, user, blog, ai, search
-
-
-
 
 
 app = FastAPI(
@@ -13,14 +9,19 @@ app = FastAPI(
 )
 
 
+# CORS SETTINGS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+
+    # later add your deployed frontend URL here
+    # "https://your-frontend.vercel.app"
+]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,39 +29,22 @@ app.add_middleware(
 
 
 
+# ROUTES
 
-app.include_router(
-    auth.router
-)
+app.include_router(auth.router)
 
-app.include_router(
-    user.router
-)
+app.include_router(user.router)
 
+app.include_router(blog.router)
 
-app.include_router(
-    blog.router
-)
+app.include_router(ai.router)
 
-
-app.include_router(
-    ai.router
-)
-
-
-app.include_router(
-    search.router
-)
+app.include_router(search.router)
 
 
 
 @app.get("/")
 async def home():
-
     return {
-        "message":"Backend running successfully"
+        "message": "Backend running successfully "
     }
-
-
-
-
